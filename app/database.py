12143375ebeed1308ea8eeff38a7b743c7be9a33
Farhad Aman <farhad.aman@snapp.cab>
2024-01-14 19:40:34 +0330

    feat: add Helm

--- app/database.py
+++ app/database.py
@@ -4,7 +4,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://user:password@localhost/dbname")
+SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_MASTER_URL", "postgresql://user:password@localhost/dbname")
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
